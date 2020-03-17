aacb2a1858d7aaf4420a3d218703e60cdfa53e55
cautiouspotato <afshaanlondhe4@gmail.com>
2020-03-17 16:04:47 +0530

    bruh?

--- src/index.js
+++ src/index.js
@@ -7,11 +7,8 @@ const config = {
     width: 800,
     height: 600,
     physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 1200 },
-            debug: false
-        }
+        default: 'matter',
+        matter: {}
     },
     scene: {
         preload: preload,
@@ -23,7 +20,7 @@ const config = {
 // fatt oof
 const game = new Phaser.Game(config);
 let player;
-let platforms;
+const GRAVITY = 8;
 let inputs = []; // what keys are pressed
 let cursors;
 let lastTime = Date.now();;
@@ -37,20 +34,23 @@ function preload() {
 }
 
 function create() {
-    platforms = this.physics.add.staticGroup();
+    // le platforms
+    let platform = this.matter.add.image(400, 568, 'ground', null,
+        { restitution: 0.4, isStatic: true });
 
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-    platforms.create(50, 250, 'ground');
 
-    player = this.physics.add.image(100, 200, 'guy');
-    player.setCollideWorldBounds(true);
-    player.body.setMaxSpeed(400);
+    player = this.matter.add.image(400, 200, 'guy', null, { inertia: Infinity });
+    // player.body.setMaxSpeed(400);
     // le friction
-    player.setDragX(1600);
+    player.setFriction(0.3);
+    player.setFrictionAir(0.005);
+
+    // cammm
+    // this.cameras.main.setBounds(0, 0, 3000, 3000);
+    this.cameras.main.startFollow(player, true, 1, 1);
+    this.cameras.main.setZoom(1);
 
     /* player inputs amirite */
-    cursors = this.input.keyboard.createCursorKeys();
-
     document.addEventListener('keydown', (event) => {
         if (!inputs.includes(event.key)) inputs.push(event.key);
     });
@@ -58,8 +58,6 @@ function create() {
         let idx = inputs.indexOf(event.key);
         if (idx > -1) inputs.splice(idx, 1);
     });
-
-    this.physics.add.collider(player, platforms);
 }
 
 function update() {
@@ -68,28 +66,26 @@ function update() {
     let dt = curTime - lastTime;
     lastTime = curTime;
 
-    // TODO strafing while falling is very slow, velocity.x seems to be
-    // getting effected by the falling thing
-    // cursors.left.isDown
-    if (inputs.includes('ArrowLeft')) {
-        player.setAccelerationX(-100 * dt);
-    } else if (inputs.includes('ArrowRight')) {
-        player.setAccelerationX(100 * dt);
+
+    if (inputs.includes('ArrowLeft') && Math.abs(player.body.velocity.x) < 5) {
+        player.applyForce({ x: -0.005, y: 0 });
+    } else if (inputs.includes('ArrowRight') && Math.abs(player.body.velocity.x) < 5) {
+        player.applyForce({ x: 0.005, y: 0 });
     } else {
-        player.setAccelerationX(0);
+        player.applyForce({ x: 0, y: 0 });
     }
 
-    if (inputs.includes('ArrowUp') && player.body.touching.down) {
-        player.setVelocityY(-Math.pow(10, 5));
+    if (inputs.includes('ArrowUp') && Math.abs(player.body.velocity.y) < 5) {
+        player.applyForce({x: 0, y: -0.01});
     }
 
     // fall faster than you rise
     // if falling increase gravity by fallmult
-    if (player.body.velocity.y > 0) {
-        player.body.velocity.y *= config.physics.arcade.gravity.y
+    if (player.body.velocity.y < 0) {
+        player.body.velocity.y *= GRAVITY
             * fallMult * dt;
         // handle variable jump height by increasing gravity when button released.
-    } else if (player.body.velocity.y < 0 && !inputs.includes('ArrowUp')) {
+    } else if (player.body.velocity.y > 0 && !inputs.includes('ArrowUp')) {
         player.body.velocity.y *= -lowJumpMult;
     }
 }
